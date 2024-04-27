33a1fe46fd91d744cb14f56c276e002ab4a670ea
footprinthere <tai15515@kakao.com>
2024-04-27 13:57:53 +0900

    Implement game environment

--- game/env.py
+++ game/env.py
@@ -1,2 +1,50 @@
+from typing import Optional
+
+import torch
+
+from .board import MineSweeperBoard
+from .open_result import OpenResult
+from .env_step_result import EnvStepResult
+
+
 class MineSweeperEnv:
-    pass
+
+    def __init__(self, board_height: int, board_width: int, n_mines: int):
+        self.board = MineSweeperBoard(
+            height=board_height, width=board_width, n_mines=n_mines
+        )
+
+        self.reward_map = {
+            OpenResult.FAIL: -10,
+            OpenResult.WIN: 10,
+            OpenResult.ADJACENT: 1,
+            OpenResult.ISOLATED: -1,
+            OpenResult.DUPLICATED: -3,
+        }
+
+    def reset(self, seed: Optional[int] = None) -> torch.Tensor:
+        """Resets the environment to a new game state. Should be called before `step()`."""
+
+        self.board.reset_board(seed=seed)
+        return torch.tensor(self.board.visible_board, dtype=torch.float32)
+
+    def step(self, action: int) -> EnvStepResult:
+        x, y = action // self.board.width, action % self.board.width
+        result = self.board.open(x, y)
+
+        return EnvStepResult(
+            visible_board=self.board.visible_board,
+            reward=self.reward_map[result],
+            terminated=result in {OpenResult.FAIL, OpenResult.WIN},
+            open_result=result,
+        )
+
+    def sample_action(self) -> torch.Tensor:
+        """Returns a random action."""
+
+        return torch.randint(0, self.board.height * self.board.width, size=(1,))
+
+    def render(self) -> str:
+        """Returns a string representation of the current game state."""
+
+        return str(self.board)
---
+++ game/env_step_result.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+from .open_result import OpenResult
+
+
+@dataclass
+class EnvStepResult:
+    """The result of a step in the game environment."""
+
+    visible_board: list[list[int]]
+    reward: int
+    terminated: bool
+    open_result: OpenResult
