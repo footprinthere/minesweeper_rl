392daf4a56c2b0d80f476c267ff0bab976e40131
footprinthere <tai15515@kakao.com>
2024-04-27 18:07:58 +0900

    Add starting '_' to property names

--- game/env.py
+++ game/env.py
@@ -9,11 +9,11 @@ from .env_step_result import EnvStepResult
 class MineSweeperEnv:
 
     def __init__(self, board_height: int, board_width: int, n_mines: int):
-        self.board = MineSweeperBoard(
+        self._board = MineSweeperBoard(
             height=board_height, width=board_width, n_mines=n_mines
         )
 
-        self.reward_map = {
+        self._reward_map = {
             OpenResult.FAIL: -10,
             OpenResult.WIN: 10,
             OpenResult.ADJACENT: 1,
@@ -24,16 +24,16 @@ class MineSweeperEnv:
     def reset(self, seed: Optional[int] = None) -> list[list[int]]:
         """Resets the environment to a new game state. Should be called before `step()`."""
 
-        self.board.reset_board(seed=seed)
-        return self.board.visible_board
+        self._board.reset_board(seed=seed)
+        return self._board.visible_board
 
     def step(self, action: int) -> EnvStepResult:
-        x, y = action // self.board.width, action % self.board.width
-        result = self.board.open_cell(x, y)
+        x, y = action // self._board.width, action % self._board.width
+        result = self._board.open_cell(x, y)
 
         return EnvStepResult(
-            visible_board=self.board.visible_board,
-            reward=self.reward_map[result],
+            visible_board=self._board.visible_board,
+            reward=self._reward_map[result],
             terminated=result in {OpenResult.FAIL, OpenResult.WIN},
             open_result=result,
         )
@@ -41,12 +41,12 @@ class MineSweeperEnv:
     def sample_action(self) -> int:
         """Returns a random action."""
 
-        return random.randrange(self.board.height * self.board.width)
+        return random.randrange(self._board.height * self._board.width)
 
     def get_open_state(self) -> list[list[bool]]:
-        return self.board.open_state
+        return self._board.open_state
 
     def render(self) -> str:
         """Returns a string representation of the current game state."""
 
-        return str(self.board)
+        return str(self._board)
