1229671ae362dd2734f9ecd86103b7f0d9f42817
footprinthere <tai15515@kakao.com>
2024-04-27 18:28:45 +0900

    Add starting '_' to property names

--- model/cnn.py
+++ model/cnn.py
@@ -13,24 +13,24 @@ class MineSweeperCNN(nn.Module):
     ):
         super().__init__()
 
-        self.input = nn.Conv2d(1, n_channels, (3, 3), padding=padding)
-        self.cnns = nn.ModuleList(
+        self._input = nn.Conv2d(1, n_channels, (3, 3), padding=padding)
+        self._cnns = nn.ModuleList(
             [
                 nn.Conv2d(n_channels, n_channels, (3, 3), padding=padding)
                 for _ in range(depth)
             ]
         )
-        self.ff = nn.Linear(
+        self._ff = nn.Linear(
             in_features=n_channels * board_size, out_features=board_size
         )
 
     def forward(self, state: Tensor) -> Tensor:
         # state: (N, H, W)
         x = state.unsqueeze(1)  # (N, 1, H, W)
-        x = F.relu(self.input(x))  # (N, C, H, W)
-        for layer in self.cnns:
+        x = F.relu(self._input(x))  # (N, C, H, W)
+        for layer in self._cnns:
             x = F.relu(layer(x))
 
         x = x.reshape(x.size(0), -1)  # (N, C*H*W)
-        x = self.ff(x)
+        x = self._ff(x)
         return x
